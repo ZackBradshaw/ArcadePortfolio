c8947bcaa5a3b6e338b01e00371fe58eed1768a0
zack <zackzbradshaw@gmail.com>
2022-10-25 09:51:16 -0500

    fixed deployment issues by removing boilerplate files

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,22 +17,6 @@ const IndexPage = () => (
               <div className="tile is-ancestor character-select">
                 <div className="tile is-parent mx-6">
                   <Link
-                    href="projects"
-                    className="tile is-child diagonal-box turtle-color-r"
-                  >
-                    <div className="turtle-text has-text-centered">
-                      <p className="giext retro-font has-text-centered">
-                        Project
-                      </p>
-                    </div>
-                    <div className="turtle">
-                      <img src="assets/projects.png"></img>
-                    </div>
-                  </Link>
-                </div>
-
-                <div className="tile is-parent mx-6">
-                  <Link
                     href="about"
                     className="tile is-child diagonal-box turtle-color-y"
                   >
